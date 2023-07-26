68ae4ec44fa766beed23af4f5b7f0df0a533fba7
SonamJ <snm.jpa@gmail.com>
2023-07-25 23:53:29 -0400

    enabled redux dev tools extension

--- src/store/store.js
+++ src/store/store.js
@@ -2,22 +2,9 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 // import logger from 'redux-logger';
+import { loggerMiddleware } from '../store/middleware/logger';
 
 import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
-};
 
 const persistConfig = {
   key: 'root',
@@ -27,9 +14,16 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV !== 'production' && loggerMiddleware].filter(
+  Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+//using redux dev tools
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+console.log(composeEnhancer);
+
+//const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 //export const store = createStore(rootReducer, undefined, composedEnhancers);
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
